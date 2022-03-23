1c505e08530190d0f37b80a6886538ba9e3dc812
Marek Charvát <marekgta233@gmail.com>
2022-03-23 19:49:57 +0100

    lil tweaks

--- src/js/data.js
+++ src/js/data.js
@@ -1,56 +1,50 @@
 const DATA = {
     "item-1": {
         "name": "Polární Ježek",
-        "description": "sdfsfsfsfsd",
-        "author": "Vesmír",
+        "description": "Lorem ipsum dolor sit amet consectetur",
+        "author": "Author",
         "image": "../img/polarni-jezek.png"
     },
     "item-2": {
         "name": "Terénnní Pavouk",
-        "description": "sdfsdfsdfsdfsdfsdf",
-        "author": "Vesmír",
+        "description": "Lorem ipsum dolor sit amet consectetur",
+        "author": "Author",
         "image": "../img/terenni-pavouk.png"
     },
     "item-3": {
         "name": "Nabouraný Jednorožec",
-        "description": "sdfsdfsdfsdfsdfsdf",
-        "author": "Vesmír",
+        "description": "Lorem ipsum dolor sit amet consectetur",
+        "author": "Author",
         "image": "../img/nabourany-jednorozec.png"
     },
     "item-4": {
         "name": "Utopené Ryby",
-        "description": "sdfsdfsdfsdfsdfsdf",
-        "author": "Vesmír",
+        "description": "Lorem ipsum dolor sit amet consectetur",
+        "author": "Author",
         "image": "../img/utopene-ryby.png"
     },
     "item-5": {
         "name": "Létající Pavouci",
-        "description": "sdfsdfsdfsdfsdfsdf",
-        "author": "Vesmír",
+        "description": "Lorem ipsum dolor sit amet consectetur",
+        "author": "Author",
         "image": "../img/letajici-pavouci.png"
     },
     "item-6": {
         "name": "Suchozemský Vodník",
-        "description": "sdfsdfsdfsdfsdfsdf",
-        "author": "Vesmír",
+        "description": "Lorem ipsum dolor sit amet consectetur",
+        "author": "Author",
         "image": "../img/suchozemsky-vodnik.png"
-    },
-    "item-7": {
-        "name": "Beruška v combiku",
-        "description": "sdfsdfsdfsdfsdfsdf",
-        "author": "Vesmír",
-        "image": "../img/beruska_v_combiku.png"
     },
     "item-8": {
         "name": "Kapesní Žebřík",
-        "description": "sdfsdfsdfsdfsdfsdf",
-        "author": "Vesmír",
+        "description": "Lorem ipsum dolor sit amet consectetur",
+        "author": "Author",
         "image": "../img/kapesni_zebrik.png"
     },
     "item-9": {
         "name": "Agent s teplou vodou",
-        "description": "sdfsdfsdfsdfsdfsdf",
-        "author": "Vesmír",
+        "description": "Lorem ipsum dolor sit amet consectetur",
+        "author": "Author",
         "image": "../img/agent_s_teplou_vodou.png"
     }
 } 

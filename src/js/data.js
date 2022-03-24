3e1f9273a954f7d3769dab1626506d8135acab5f
Tomáš Plánský <92803012+tomas-plansky@users.noreply.github.com>
2022-03-24 08:27:58 +0100

    Fix image links

--- src/js/data.js
+++ src/js/data.js
@@ -3,48 +3,48 @@ const DATA = {
         "name": "Polární Ježek",
         "description": "Lorem ipsum dolor sit amet consectetur",
         "author": "Author",
-        "image": "../img/img-1.jpg"
+        "image": "./img/img-1.jpg"
     },
     "item-2": {
         "name": "Terénnní Pavouk",
         "description": "Lorem ipsum dolor sit amet consectetur",
         "author": "Author",
-        "image": "../img/img-1.jpg"
+        "image": "./img/img-1.jpg"
     },
     "item-3": {
         "name": "Nabouraný Jednorožec",
         "description": "Lorem ipsum dolor sit amet consectetur",
         "author": "Author",
-        "image": "../img/img-1.jpg"
+        "image": "./img/img-1.jpg"
     },
     "item-4": {
         "name": "Utopené Ryby",
         "description": "Lorem ipsum dolor sit amet consectetur",
         "author": "Author",
-        "image": "../img/img-1.jpg"
+        "image": "./img/img-1.jpg"
     },
     "item-5": {
         "name": "Létající Pavouci",
         "description": "Lorem ipsum dolor sit amet consectetur",
         "author": "Author",
-        "image": "../img/img-1.jpg"
+        "image": "./img/img-1.jpg"
     },
     "item-6": {
         "name": "Suchozemský Vodník",
         "description": "Lorem ipsum dolor sit amet consectetur",
         "author": "Author",
-        "image": "../img/img-1.jpg"
+        "image": "./img/img-1.jpg"
     },
     "item-8": {
         "name": "Kapesní Žebřík",
         "description": "Lorem ipsum dolor sit amet consectetur",
         "author": "Author",
-        "image": "../img/img-1.jpg"
+        "image": "./img/img-1.jpg"
     },
     "item-9": {
         "name": "Agent s teplou vodou",
         "description": "Lorem ipsum dolor sit amet consectetur",
         "author": "Author",
-        "image": "../img/img-1.jpg"
+        "image": "./img/img-1.jpg"
     }
 } 
